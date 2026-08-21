37293beacd60164c8c505bf6efc747b2bfafd4d2
shammir-ahmed <mdshammirahmedanik@gmail.com>
2026-08-22 01:27:47 +0600

    Bump version to 1.2.0 for Artauk layout updates

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( 'MODERNBLOG_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( 'MODERNBLOG_VERSION', '1.1.0' );
+	define( 'MODERNBLOG_VERSION', '1.2.0' );
 }
 
 if ( ! function_exists( 'modernblog_setup' ) ) :
